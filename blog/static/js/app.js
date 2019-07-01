dea14d33925c6b5f69e2e2407e4bd337aaa1a8fd
remmi11 <wtgeographer@gmail.com>
2019-07-01 11:42:23 -0500

    init

--- blog/static/js/app.js
+++ blog/static/js/app.js
@@ -1,11 +1,7 @@
 
 L.mapbox.accessToken = 'pk.eyJ1Ijoid3RnZW9ncmFwaGVyIiwiYSI6ImNpdGFicWJqYjAwdzUydHM2M2g0MmhsYXAifQ.oO-MYNUC2tVeXa1xYbCIyw';
 
-
-// var map = L.mapbox.map('map', {
-//     layers: [sections, contours]
-// }).setView([35.058104, -101.749877], 9);
-
+// tiles
 var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -13,10 +9,15 @@ var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</
 
 var grayscale = L.tileLayer(mbUrl, { id: 'mapbox.light', attribution: mbAttr }),
     streets = L.tileLayer(mbUrl, { id: 'mapbox.streets', attribution: mbAttr })
-var satellite = L.mapbox.styleLayer('mapbox://styles/mapbox/satellite-streets-v9');
 
+var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+// styles
+var satellite = L.mapbox.styleLayer('mapbox://styles/mapbox/satellite-streets-v9');
 var sections = L.mapbox.styleLayer('mapbox://styles/wtgeographer/cjf442uso0z3e2ss1w8jpnyp5');
-var contours = L.mapbox.styleLayer('mapbox://styles/wtgeographer/cjf6xjfak3ebb2sobig2fnpzh');
+// var contours = L.mapbox.styleLayer('mapbox://styles/wtgeographer/cjf6xjfak3ebb2sobig2fnpzh');
 var prad = L.mapbox.styleLayer('mapbox://styles/wtgeographer/cjf75lnp62m612smws69qnu4o');
 var floods = L.mapbox.styleLayer('mapbox://styles/wtgeographer/cjf9riogz4z8n2rmk4eawkc6o');
 var places = L.mapbox.styleLayer('mapbox://styles/wtgeographer/cjftuokfx8ime2sqpyhj88q67');
@@ -35,12 +36,13 @@ measureControl.addTo(map);
 
 var baseLayers = {
     "Streets": streets,
-    "Satellite": satellite
+    "Satellite": satellite,
+    "Mapnik": OpenStreetMap_Mapnik
 };
 
 var overlays = {
     "Sections": sections,
-    "10ft Contours": contours,
+    // "10ft Contours": contours,
     "Prad Lines": prad,
     "Flood Hazards": floods,
     "City Limits": places
