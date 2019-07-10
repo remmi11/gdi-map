b0b7d9db075a19c0bd1ea8a2818b9d7a5b451e9a
remmi11 <wtgeographer@gmail.com>
2019-07-10 14:59:27 -0500

    init

--- blog/static/js/app.js
+++ blog/static/js/app.js
@@ -23,7 +23,7 @@ var layer = 'furmanrecords:section_lines_wgs84';
 var projection_epsg_no = '4326';
 //"http://13.82.41.95:8080/geoserver/gwc/service/tms/1.0.0/furmanrecords%3Asection_lines_wgs84@EPSG%3A4326@png"
 //var url = 'http://13.82.41.95:8080/geoserver/gwc/service/tms/1.0.0/' + layer + '@EPSG%3A' + projection_epsg_no + '@pbf/{z}/{x}/{-y}.pbf'
-var url = 'https://13.82.41.95:8080/geoserver/gwc/service/tms/1.0.0/furmanrecords%3Asection_lines_wgs84@EPSG%3A4326@png'
+var url = 'https://gs.furmanrecords.com/geoserver/gwc/service/tms/1.0.0/furmanrecords%3Asection_lines_wgs84@EPSG%3A4326@png'
 
 //var mapboxUrl = "https://{s}.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6/{z}/{x}/{y}.vector.pbf?access_token={token}";
 
